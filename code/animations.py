cf94030659f8fb7c2805fe56b2a404cb6de4428e
ThijsAlens <160174787+ThijsAlens@users.noreply.github.com>
2024-04-20 11:31:07 +0200

    extra animation

--- code/animations.py
+++ code/animations.py
@@ -14,17 +14,25 @@ def create_values(easeFunction, frequency):
     return values
 
 def send_startup_sequence():
-    values = create_values(startup_function, 750)
+    values = create_values(startup_function, 800)
+    reverse = values[::-2]
 
-    finalLRValues = list(np.zeros(1000))
-    finalMValues = list(np.zeros(1000))
-    finalLRValues[:749] = values
-    finalMValues[250:999] = values
+    finalLRValues = list(np.zeros(1400))
+    finalMValues = list(np.zeros(1400))
+
+    finalLRValues[:799] = values
+    finalLRValues[900:919] = np.ones(20)
+    finalLRValues[1000:] = reverse
+    
+    finalMValues[400:799] = values[:399]
+    finalMValues[900:919] = np.ones(20)
+    finalMValues[1000:] = reverse
+
     finalTotal = np.array([finalLRValues, finalMValues, finalMValues, finalLRValues])
 
     if (not Data_Processing.writingToActuators):
         Data_Processing.writingToActuators = True
-        for i in range(1000):
+        for i in range(1400):
             Data_Processing.write_serial(np.array(finalTotal[0][i], finalTotal[1][i], finalTotal[2][i], finalTotal[3][i]))
             time.sleep(0.001)
         Data_Processing.writingToActuators = False
